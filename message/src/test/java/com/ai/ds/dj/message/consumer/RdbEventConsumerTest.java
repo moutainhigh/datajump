e0b61bae58be50a6458951535133d7a8466aa1bc
wuwenhui0917 <wuwenhui0917@163.com>
2019-02-27 15:06:31 +0800

    拆分工程新增测试用例

---
+++ message/src/test/java/com/ai/ds/dj/message/consumer/RdbEventConsumerTest.java
@@ -0,0 +1,23 @@
+package com.ai.ds.dj.message.consumer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Copyright asiainfo.com
+ *
+ * @author wuwh6
+ */
+public class RdbEventConsumerTest {
+
+    RdbEventConsumer consumer = new RdbEventConsumer();
+
+    @Test
+    public void handleString() throws Exception {
+
+    }
+
+
+
+}
